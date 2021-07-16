02e063a02893910ef435ee8e82cdc4f6e4590fcb
gosha-portfolio <jackie12197394@gmail.com>
2021-07-16 16:48:09 +0300

    portfollio

---
+++ dist/pulse/js/script.js
@@ -0,0 +1,123 @@
+$(document).ready(function () {
+    $('.corousel__inner').slick({
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.png"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: false,
+                arrows: false
+            }
+        }]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.price__button').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: {
+                    required: true,
+                    minlength: 6
+                },
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Поле имя обязательно для заполнения",
+                    minlength: jQuery.validator.format("Длина имени должна быть больше 2-ух символов")
+                },
+                phone: {
+                    required: "Поле телефон обязательно для заполнения",
+                    minlength: jQuery.validator.format("Номер телефона должен быть больше 5-ти символов")
+                },
+                email: {
+                    required: "Введите Вашу почту",
+                    email: "Ваша почта должна быть в формате - name@domain.com"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#promo-form');
+    validateForms('#order-form');
+
+    $('input[name=phone').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn('slow');
+        } else
+            $('.pageup').fadeOut('slow');
+    });
+
+    $("a[href^='#']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+
+    new WOW().init();
+});
